3def8acfb711b50b2529b33cc8a6ba01d9493475
Feliciano Santana <fsfelicianosantana@gmail.com>
2015-10-30 23:29:03 -0300

    Adds new project folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var outline = require('./outline.json');
 
 gulp.task('default', ['build', 'watch']);
 gulp.task('build', ['style', 'script', 'index']);
@@ -9,16 +10,19 @@ gulp.task('script', ScriptTask);
 gulp.task('index', IndexTask);
 gulp.task('reload-browser', ReloadBrowserTask);
 
-function WatchTask () {
-
-}
+var concat = require('gulp-concat');
+var gutil = require('gulp-util');
 
 function StyleTask () {
-
+	return gulp.src(outline.src + '/css/**/*.css')
+			.pipe(concat(withMinCSS(outline.name))).on('error', gutil.log)
+			.pipe(gulp.dest(outline.dist + '/css/')).on('error', gutil.log);
 }
 
 function ScriptTask () {
-
+	return gulp.src(outline.src + '/js/**/*.js')
+			.pipe(concat(withMinJS(outline.name))).on('error', gutil.log)
+			.pipe(gulp.dest(outline.dist + '/css/')).on('error', gutil.log);
 }
 
 function IndexTask () {
@@ -27,4 +31,16 @@ function IndexTask () {
 
 function ReloadBrowserTask () {
 
+}
+
+function WatchTask () {
+
+}
+
+function withMinJS (file) {
+	return file + ".min.js";
+}
+
+function withMinCSS (file) {
+	return file + ".min.css";
 }
